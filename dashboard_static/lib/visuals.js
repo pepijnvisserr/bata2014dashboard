f1158fb5b8109cace53d4248874e283bb5578a51
pepijnvisserr <57093030+pepijnvisserr@users.noreply.github.com>
2019-10-29 16:22:42 +0100

    Add files via upload
    word cloud + graph + announcements work

--- dashboard_static/lib/visuals.js
+++ dashboard_static/lib/visuals.js
@@ -1,25 +1,40 @@
 $(document).ready(function(){
     $("#visuals div").addClass("invisible");
     $(".Wordcloud").removeClass("invisible");
-    
-    $("#btn_cloud").click(function(){        
-            $("#visuals div").addClass("invisible");
+    $(".Wordcloud div").removeClass("invisible");
+
+    $("#btn_cloud").click(function(){ 
+        $("#visuals > div").addClass("invisible");
             $(".Wordcloud").removeClass("invisible");
+            $(".Wordcloud div").removeClass("invisible");
+            $("#wolk").addClass("grid_8 vert_8");
+            $("#graph").removeClass("grid_8 vert_8");
+            //$("#geolocation").removeClass("grid_8 vert_4");
+
             $("*li.nav-item a").removeClass("active");
             $(this).addClass("active");
     });
     
     $("#btn_geo").click(function(){
-            $("#visuals div").addClass("invisible");
+        $("#visuals > div").addClass("invisible");
             $(".Geolocation").removeClass("invisible");
+            $(".Geolocation div").removeClass("invisible");
+            $("#wolk").removeClass("grid_8 vert_8");
+            $("#graph").removeClass("grid_8 vert_8");
+            //$("#geolocation").addClass("grid_8 vert_4");
+
             $("*li.nav-item a").removeClass("active");
             $(this).addClass("active");
     });
     
     $("#btn_graph").click(function(){
-            $("#visuals div").addClass("invisible");
+        $("#visuals > div").addClass("invisible");
             $(".Graph").removeClass("invisible");
             $(".Graph div").removeClass("invisible");
+            $("#wolk").removeClass("grid_8 vert_8");
+            $("#graph").addClass("grid_8 vert_8");
+            //$("#geolocation").removeClass("grid_8 vert_4");
+
             $("*li.nav-item a").removeClass("active");
             $(this).addClass("active");
     });
