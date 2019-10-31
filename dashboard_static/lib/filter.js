c81dc632631add026268d8ce8c1dc0ce3c5cb419
MatthiasWentink1 <57093432+MatthiasWentink1@users.noreply.github.com>
2019-10-31 15:25:21 +0100

    Filter 2.0

--- dashboard_static/lib/filter.js
+++ dashboard_static/lib/filter.js
@@ -1,107 +1,195 @@
 $(document).ready(function(){
-
-	target = "";
-	operation = "";
-	$(".filter").click(function(){
+	name = "";
+	loc = "";
+	content = "";
+	language = "";
+	
+	pattern1 = "";
+	pattern2 = "";
+	pattern3 = "";
+	pattern4 = "";
+	
+	$(".filterbtn").click(function(){
 		$("*.tweet-list li").removeClass("filtered");
-		target = $(".field1").val();
-		operation = $(".field2").val();
-		pattern = new RegExp(target, 'i');
 		console.log("click");
-		switch(operation){
-			//Content
-			case "1":
-				$("*.tweet-list li").each(function(){
-					n = $(this).find("p").text();
-					res = n.match(pattern);
-					console.log(pattern);
-					if ($(res).length){
-						
-					} else {
-						$(this).addClass("filtered");
-					}
-				});
-			break;
-			
-			//Hashtag
-			case "2":
-				$("*.tweet-list li").each(function(){
-					n = "#" + $(this).find("p").text();
-					res = n.match(pattern);
-					console.log(pattern);
-					if ($(res).length){
-						
-					} else {
-						$(this).addClass("filtered");
-					}
-				});
-			break;
-			
-			//Author
-			case "3":
-				$("*.tweet-list li").each(function(){
-					n = $(this).find("strong").text();
-					res = n.match(pattern);
-					console.log(pattern);
-					if ($(res).length){
-						
-					} else {
-						$(this).addClass("filtered");
-					}
-				});
-			break;
-			default:
-				console.log("error");
+		name = $("#field1").val();
+		loc = $("#field2").val();
+		content = $("#field3").val();
+		language = $("#field4").val();
+		
+		pattern1 = new RegExp(name, 'i');
+		pattern2 = new RegExp(loc, 'i');
+		pattern3 = new RegExp(content, 'i');
+		pattern4 = new RegExp(language, 'i');
+		
+		function nempty(x){
+			if (x == "") {
+				console.log("false");
+				return false;
+			}
+			else {
+				console.log("true");
+				return true;
+			}
 		}
-	});
-	$("body").on('DOMSubtreeModified',"#tweet",function(){
-		//target = $(".field1").val();
-		//operation = $(".field2").val();
-		pattern = new RegExp(target, 'i');
-		switch(operation){
-			//Content
-			case "1":
-				$(".tweet-list li:first").each(function(){
-					n = $(this).find("p").text();
-					res = n.match(pattern);
-					console.log("1");
-					if ($(res).length){
-						
-					} else {
-						$(this).addClass("filtered");
-					}
-				});
-			break;
 			
-			//Hashtag
-			case "2":
-				$(".tweet-list li:first").each(function(){
-					n = "#" + $(this).find("p").text();
-					res = n.match(pattern);
-					console.log("2");
-					if ($(res).length){
-						
-					} else {
-						$(this).addClass("filtered");
-					}
-				});
-			break;
-			
-			//Author
-			case "3":
-				$(".tweet-list li:first").each(function(){
-					n = $(this).find("strong").text();
-					res = n.match(pattern);
-					console.log("3");
-					if ($(res).length){
-						
-					} else {
-						$(this).addClass("filtered");
-					}
-				});
-			break;
-			default:
-				console.log("error");
+		function checkName(x) {
+			n = $(x).find(".fullname").text();
+			res = n.match(pattern1);
+			if ($(res).length){
+				return true;
+			} else {
+				return false;
+			}
 		}
+		
+		function checkLoc(x) {
+			n = $(x).find(".loc").text();
+			res = n.match(pattern2);
+			if ($(res).length){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		function checkContent(x) {
+			n = $(x).find("p").text();
+			res = n.match(pattern3);
+			if ($(res).length){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		function checkLang(x) {
+			n = $(x).find(".lang").text();
+			res = n.match(pattern4);
+			if ($(res).length){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		
+		$("*.tweet-list li").each(function(){
+			current = $(this);
+			console.log(current);
+			/*
+			if(nempty(name) && checkName(current)){
+				console.log(nempty(name))
+				console.log(checkName())
+				//console.log("found Name");
+			}
+			else if(nempty(loc) && checkLoc(current)){
+				console.log("found Location");
+			}
+			else if(nempty(content) && checkContent(current)){
+				console.log("found content");
+			}
+			else if(nempty(language) && checkLang(current)){
+				console.log("found Language");
+			}
+			else {
+				console.log("not found");
+				$(this).addClass("filtered");
+			}
+			*/
+			
+			if(checkName(current) && checkLoc(current) && checkContent(current) && checkLang(current)) {
+				console.log("found");
+			} else {
+				$(this).addClass("filtered");
+				console.log("not found");
+			}
+		});
 	});
-	});
+	
+	$("body").on('DOMSubtreeModified','#tweet',function() {
+    	function nempty(x){
+			if (x == "") {
+				console.log("false");
+				return false;
+			}
+			else {
+				console.log("true");
+				return true;
+			}
+		}
+			
+		function checkName(x) {
+			n = $(x).find(".fullname").text();
+			res = n.match(pattern1);
+			if ($(res).length){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		function checkLoc(x) {
+			n = $(x).find(".loc").text();
+			res = n.match(pattern2);
+			if ($(res).length){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		function checkContent(x) {
+			n = $(x).find("p").text();
+			res = n.match(pattern3);
+			if ($(res).length){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		function checkLang(x) {
+			n = $(x).find(".lang").text();
+			res = n.match(pattern4);
+			if ($(res).length){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		
+		$("*.tweet-list li").each(function(){
+			current = $(this);
+			console.log(current);
+			/*
+			if(nempty(name) && checkName(current)){
+				console.log(nempty(name))
+				console.log(checkName())
+				//console.log("found Name");
+			}
+			else if(nempty(loc) && checkLoc(current)){
+				console.log("found Location");
+			}
+			else if(nempty(content) && checkContent(current)){
+				console.log("found content");
+			}
+			else if(nempty(language) && checkLang(current)){
+				console.log("found Language");
+			}
+			else {
+				console.log("not found");
+				$(this).addClass("filtered");
+			}
+			*/
+			
+			if(checkName(current) && checkLoc(current) && checkContent(current) && checkLang(current)) {
+				console.log("found");
+			} else {
+				$(this).addClass("filtered");
+				console.log("not found");
+			}
+		});
+    });
+});
